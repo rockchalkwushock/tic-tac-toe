9bf50a11f4bebfefd8ef1264093786fd1e18edfa
Cody Brunner <me@codybrunner.com>
2022-06-25 17:48:12 -0500

    chore(root): prunes repo of what I don't need

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,13 @@
 import { ErrorBoundary } from '@components/ErrorBoundary'
 import { LoadingOverlay } from '@components/LoadingOverlay'
-import { BaseLayout } from '@layouts/BaseLayout'
-import { Route, Routes } from '@services/router'
-import { lazy, Suspense } from 'react'
-
-const Home = lazy(
-	async () => import('./pages/Home/Home' /* webpackChunkName Home */)
-)
-const NotFound = lazy(
-	async () =>
-		import('./pages/NotFound/NotFound' /* webpackChunkName NotFound */)
-)
+import { Home } from '@pages/Home'
+import { Suspense } from 'react'
 
 function App() {
 	return (
 		<ErrorBoundary>
 			<Suspense fallback={<LoadingOverlay />}>
-				<Routes>
-					<Route element={<BaseLayout />}>
-						<Route path='/' element={<Home />} />
-						{/* Catch All */}
-						<Route path='*' element={<NotFound />} />
-					</Route>
-				</Routes>
+				<Home />
 			</Suspense>
 		</ErrorBoundary>
 	)
